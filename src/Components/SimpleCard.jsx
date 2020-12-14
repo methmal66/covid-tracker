c3ab449b16e7389dcb70189e38c3b4f489d362db
Sanuja Methmal <methmal66@yahoo.com>
2020-12-14 10:15:25 +0530

    refactor using material-ui grid laylout

--- src/Components/SimpleCard.jsx
+++ src/Components/SimpleCard.jsx
@@ -1,19 +1,12 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 
-const useStyles = makeStyles({
-  root: {},
-});
-
 const SimpleCard = ({ title, _new, total }) => {
-  const classes = useStyles();
-
   return (
     <div>
-      <Card className={classes.root}>
+      <Card>
         <CardContent>
           <Typography>{title}</Typography>
           <Typography variant="h5">+{_new} today</Typography>
